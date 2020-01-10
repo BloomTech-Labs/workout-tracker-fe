baf5e2bd6b70dd04f00592b02a74a549e283141c
heuzin <matheusqs.arq@gmail.com>
2020-01-09 21:57:22 -0300

    Working on background color and text color

--- frontend/src/components/FullPageIntroWithFixedNavbar.js
+++ frontend/src/components/FullPageIntroWithFixedNavbar.js
@@ -193,11 +193,11 @@ class FullPageIntroWithFixedNavbar extends React.Component {
     </MDBContainer>
     
     <div className='container'>
-    <p align="justify">
-      The FitnessBliss fitness web app lets you create, print, track and chart workout routines on your own.
-      By giving you access to powerful workout routines creation tools and a huge exercise database, FitnessBliss
+    <p align="justify" className='about-app'>
+      First Rep workout app lets you create, print, track and chart workout routines on your own.
+      By giving you access to powerful workout routines creation tools and a huge exercise database, First Rep
       will enable you to take control of your workouts by letting you create effective workout routines. 
-      Whether you are using the target muscle workout routine wizard or one of the many pre-built workout routines 
+      Whether you are using the target muscle workout routine or one of the many pre-built workout routines 
       for sports or specific body areas, you are sure to find a way to create workout routines that work for you.
       </p>      
     </div>
--- frontend/src/styles/FeaturesSection.js
+++ frontend/src/styles/FeaturesSection.js
@@ -6,15 +6,6 @@ const FeaturesStyleSection = styled.div`
       height: 500px;
    }
 
-   .about-app {
-      background-image: linear-gradient(to top,#2c2c54,rgb(76,92,150));
-      padding-top: 40px;
-      padding-bottom: 40px;
-   }
-
-   .about-app {
-      color: white;
-   }
 `;
 
 export default FeaturesStyleSection;
