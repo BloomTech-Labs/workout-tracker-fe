f4b56243d165873241cc652cdacda7ce8e8017f1
heuzin <matheusqs.arq@gmail.com>
2020-01-14 22:59:31 -0300

    calories lost graph design added a second month possibility

--- frontend/src/components/Profile/CaloriesGraph.js
+++ frontend/src/components/Profile/CaloriesGraph.js
@@ -10,7 +10,7 @@ class ChartsPage extends React.Component {
       labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
       datasets: [
         {
-          label: "Calories",
+          label: "First Month",
           fill: true,
           lineTension: 0.3,
           backgroundColor: "rgba(184, 185, 210, .3)",
@@ -29,6 +29,27 @@ class ChartsPage extends React.Component {
           pointRadius: 1,
           pointHitRadius: 10,
           data: [28, 48, 40, 19]
+        },
+        {
+          label: "Second Month",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(225, 204,230, .3)",
+          borderColor: "rgb(205, 130, 158)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(205, 130,1 58)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81]
         }
       ]
     }
