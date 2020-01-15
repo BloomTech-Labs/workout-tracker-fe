e6498734dfb60c20cedd9c88f9b628467195c818
heuzin <matheusqs.arq@gmail.com>
2020-01-14 22:00:08 -0300

    Graph design for muscle worked mothly

--- frontend/src/components/Profile/ProgressGraph.js
+++ frontend/src/components/Profile/ProgressGraph.js
@@ -1,55 +1,130 @@
-import React, { Component } from "react";
+import React from "react";
 import { Line } from "react-chartjs-2";
+import { MDBContainer } from "mdbreact";
 import "./Profile.css";
 
-export default class ProgressGraph extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: {
-        labels: [
-          "Week 1",
-          "Week 2",
-          "Week 3",
-          "Week 4",
-          "Week 5",
-          "Week 6",
-          "Week 7"
-        ],
-        datasets: [
-          {
-            label: "Muscle Groups Worked",
-            backgroundColor: "rgb(13, 90, 190, .5)",
-            data: [3, 2, 5, 2, 3, 4, 2, 0]
-          }
-        ]
-      }
-    };
-  }
-
-  getChartData = canvas => {
-    const data = this.state.data;
-    if (data.datasets) {
-      data.datasets.forEach(set => {
-        set.borderColor = "black";
-        set.borderWidth = 2;
-        set.lineTension = 0.1;
-      });
+class ChartsPage extends React.Component {
+  state = {
+    dataLine: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Chest",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(225, 204,230, .3)",
+          borderColor: "rgb(205, 130, 158)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(205, 130, 158)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: "Biceps",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(184, 185, 210, .3)",
+          borderColor: "rgb(35, 26, 136)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgb(35, 26, 136)",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [8, 40, 30, 9, 85, 27,80]
+        },
+        {
+          label: "Triceps",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(124, 164, 190, .3)",
+          borderColor: "yellow",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "yellow",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [0, 70, 53, 90, 15, 65, 4]
+        },
+        {
+          label: "Deltoid",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(100, 145, 170, .3)",
+          borderColor: "olive",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "olive",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [100, 50, 70, 68, 64, 59, 40]
+        },
+        {
+          label: "Legs",
+          fill: true,
+          lineTension: 0.3,
+          backgroundColor: "rgba(70, 125, 150, .3)",
+          borderColor: "green",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "green",
+          pointBackgroundColor: "rgb(255, 255, 255)",
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgb(0, 0, 0)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [0, 20, 30, 48, 64, 79, 90]
+        }
+      ]
     }
-    return data;
   };
 
   render() {
     return (
-      <div className="progress-graph-container">
-        <Line
-          options={{
-            responsive: true
-          }}
-          data={this.getChartData}
-        />
-      </div>
+      <MDBContainer>
+        <h3 className="mt-5">Muscle Group Worked</h3>
+        <Line data={this.state.dataLine} options={{ responsive: true }} />
+      </MDBContainer>
     );
   }
 }
+
+export default ChartsPage;
