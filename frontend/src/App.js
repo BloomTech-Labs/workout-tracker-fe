07aa17ab479ed318927bde3dcb39b167e86542fe
heuzin <matheusqs.arq@gmail.com>
2020-01-17 21:20:14 -0300

    merge from master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,18 +9,12 @@ import "./App.css";
 import Graphs from "./components/Profile/Graphs/Graphs";
 import FullPageIntroWithFixedNavbar from "./components/FullPageIntroWithFixedNavbar";
 import AutocompletePage from "./components/AutoCompletePage";
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 06c008c47270025e101f5c45bd58097e0c9b0c2e
 import Routines from './components/Routines/Routines.js';
-import MoreInfo from "./components/Profile/MoreProfileInfo"
 
 function App() {
   return (
     <div className="App">
-      <link href="https://fonts.googleapis.com/css?family=Alatsi&display=swap" rel="stylesheet" />
+              <link href="https://fonts.googleapis.com/css?family=Alatsi&display=swap" rel="stylesheet" />
       <Router>
         <Router>
           {/* Nav Bar */}
@@ -34,36 +28,14 @@ function App() {
               exact
               render={props => <Graphs {...props} />}
             />
-            <Route path="/profile-info" component={MoreInfo} />
-            </Switch>
-        {/*Routines*/}
-        {/* <Route path='/routines' exact render={(props) => <Routines {...props} />}/> */}
-        <Routines />
 
-       
-        
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/login" component={Login} />
-<<<<<<< HEAD
-
-            
-
-            {/*Routines*/}
-            <Routines />
-
-            {/* Footer */}
-            <div className='footer'>
-            </div>
-
-            
-          
-          </Router>
-=======
             <Route exact path="/features" component={Features} />
             <Route exact path="/aboutus" component={AboutUs} />
           </Switch>
->>>>>>> e8d784ef038b4cf7797fa0ad391a3a3d98e2786a
         </Router>
+      </Router>
     </div>
   );
 }
