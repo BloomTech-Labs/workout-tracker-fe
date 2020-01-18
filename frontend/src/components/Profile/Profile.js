fa295cdbe02e254a29e4231038f3bbdd17603408
heuzin <matheusqs.arq@gmail.com>
2020-01-17 22:52:33 -0300

    names displayin locally

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react"; 
 import "./Profile.css";
 import ProfileNavbar from './ProfileNavBar'
+import MemberProfile from './MemberProfile'
+import UserInfo from './UserInfo'
 
 export default class Profile extends Component {
   render() {
     return (
       <div className="profile-container">
         <ProfileNavbar />
+        <UserInfo />
       </div>
     );
   }
