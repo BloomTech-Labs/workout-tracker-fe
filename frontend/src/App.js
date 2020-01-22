fb303a25a0d8920203a688b17a162b19805aa6e3
Rod <rod.bromberg@yahoo.com>
2020-01-22 18:21:11 -0500

    basic multi input form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,29 +7,41 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Modal from "./components/Modal";
 import "./App.css";
 import Graphs from "./components/Profile/Graphs/Graphs";
-import Profile from "./components/Profile/Profile"
+import Profile from "./components/Profile/Profile";
 import FullPageIntroWithFixedNavbar from "./components/FullPageIntroWithFixedNavbar";
 import AutocompletePage from "./components/AutoCompletePage";
-import Routines from './components/Routines/Routines.js';
+import Routines from "./components/Routines/Routines.js";
+import RecordRoutine from "./components/Routines/RecordRoutine";
 
 function App() {
   return (
     <div className="App">
-              <link href="https://fonts.googleapis.com/css?family=Alatsi&display=swap" rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css?family=Alatsi&display=swap"
+        rel="stylesheet"
+      />
       <Router>
         <Router>
           {/* Nav Bar */}
-          <div>
-          </div>
-          
+          <div></div>
+
           <Switch>
             <Route exact path="/" component={FullPageIntroWithFixedNavbar} />
-            <Route path="/graphs" exact render={props => <Graphs {...props} />} />
-            <Route path="/profile" exact render={props => <Profile {...props} />} />
+            <Route
+              path="/graphs"
+              exact
+              render={props => <Graphs {...props} />}
+            />
+            <Route
+              path="/profile"
+              exact
+              render={props => <Profile {...props} />}
+            />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/features" component={Features} />
             <Route exact path="/aboutus" component={AboutUs} />
+            <Route exact path="/routine" component={RecordRoutine} />
           </Switch>
         </Router>
       </Router>
