939d2cc626ef824e2947e09ddc12864bd2ddb61c
Rod <rod.bromberg@yahoo.com>
2020-01-28 21:59:57 -0500

    display routines routing in progress

--- frontend/src/components/Routines/DisplayRoutines.js
+++ frontend/src/components/Routines/DisplayRoutines.js
@@ -4,21 +4,28 @@ import axios from "axios";
 
 function DisplayRoutines() {
   const [data, setData] = useState([{}]);
-
+  const [id, setId] = useState({});
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
         "https://firstrep.herokuapp.com/api/routines/"
       );
       setData(result.data);
+      setId(result.data);
     };
     fetchData();
   }, []);
 
+  const obj = {};
+
+  // onClick={obj['routine_name']}
+
+  // counter
   return (
     <ul>
+      {console.log("id is ", id)}
       {data.map(a => (
-        <li>{a.routine_name}</li>
+        <li onClick={{}}>{a.routine_name}</li>
       ))}
     </ul>
   );
